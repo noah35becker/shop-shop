c621fbc0caf995d73023306969b64ae39db9fc07
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-11-01 15:16:50 -0400

    add global state

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -9,7 +9,7 @@ import {reducer} from '../utils/reducers';
 const initialState = {
     products: [],
     categories: [{name: 'Food'}],
-    currentCategory: '1'
+    currentCategory: 1
 };
 
 
@@ -39,9 +39,9 @@ test('UPDATE_CATEGORIES', () => {
 test('UPDATE_CURRENT_CATEGORY', () => {
     let newState = reducer(initialState, {
         type: UPDATE_CURRENT_CATEGORY,
-        currentCategory: '2'
+        currentCategory: 2
     });
 
-    expect(newState.currentCategory).toBe('2');
-    expect(initialState.currentCategory).toBe('1');
+    expect(newState.currentCategory).toBe(2);
+    expect(initialState.currentCategory).toBe(1);
 });
---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,20 @@
+
+import React, {createContext, useContext} from 'react';  // React MUST be imported here
+import {useProductReducer} from './reducers';
+
+
+const StoreContext = createContext();
+const {Provider} = StoreContext;
+
+
+export const StoreProvider = ({value = [], ...props}) => {
+    const [state, dispatch] = useProductReducer({
+        products: [],
+        categories: [],
+        currentCategory: null,
+    });
+    
+    return <Provider value={[state, dispatch]} {...props} />;  // `props` is necessary here because this Provider will wrap all other elements on the page; `props.children` will allow access to those other elements
+}
+
+export const useStoreContext = () => useContext(StoreContext);
